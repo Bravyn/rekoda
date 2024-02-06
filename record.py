3e3565e4a35d9cd247021c868d939b1b1871ead5
Ian Bravyn <ianbravynsa@gmail.com>
2024-02-06 19:09:59 +0300

    A simpler voice recorder in python

---
+++ record.py
@@ -0,0 +1,24 @@
+import sounddevice as sd 
+import soundfile as sf
+
+from tkinter import *
+
+def voicer():
+    sample_rate = 48000
+
+    #in seconds
+    duration = 5
+    recording1 = sd.rec(int(duration * sample_rate),samplerate = sample_rate, channels=2)
+    sd.wait()
+
+    #save file
+    return sf.write("reco1.flac", recording1, sample_rate)
+
+master = Tk()
+
+Label(master, text = "Voicer").grid(row = 0, sticky=W, rowspan=5)
+
+b = Button(master, text="Start", command=voicer)
+b.grid(row = 0, column=2, columnspan=2, rowspan=2, padx=5, pady=5)
+
+mainloop()
