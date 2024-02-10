d7a07b5f39045155b31c9703edc7273e77a022c7
Ian Bravyn <ianbravynsa@gmail.com>
2024-02-10 13:12:09 +0300

    Update game.py

--- game.py
+++ game.py
@@ -39,10 +39,12 @@ platform_color = ORANGE
 #first off, our start screen
 def start_screen():
     screen.fill(BLACK)
-    show_text_on_screen("Bounc Ball", 50, HEIGHT//4)
+    show_text_on_screen("Gravity Ball", 50, HEIGHT//4)
     show_text_on_screen("Press any key to start...", 30, HEIGHT//3)
-    show_text_on_screen("Move the platform with arrow keys...", 30, HEIGHT // 2)
+    show_text_on_screen("Move the platform with arrow keys...", 25, HEIGHT // 2)
+    show_text_on_screen("Developed by Ian Bravyn", 15, HEIGHT // 8)
     pygame.display.flip()
+
     wait_for_key()
 
 def game_over():
@@ -87,3 +89,96 @@ def change_platform_color():
 
 
 start_screen()
+game_running = True
+
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+
+        keys = pygame.key.get_pressed()
+
+        #moving the platform
+        platform_pos[0] += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * platform_speed
+        platform_pos[1] += (keys[pygame.K_DOWN] - keys[pygame.K_UP] ) * platform_speed
+
+        #make the paltform constained within screen boundaries
+        platform_pos[0] = max(0, min(platform_pos[0], WIDTH - PLATFORM_WIDTH))
+        platform_pos[1] = max(0, min(platform_pos[1], HEIGHT - PLATFORM_HEIGHT))
+
+        #move the ball
+        ball_pos[0] += ball_speed[0]
+        ball_pos[1] += ball_speed[1]
+
+        #bounce of the walls
+        if ball_pos[0] <= 0 or ball_pos[0] >=   WIDTH:
+            ball_speed[0] = -ball_speed[0]
+
+        if ball_pos[1] <= 0:
+            ball_speed[1] = -ball_speed[1]
+
+        #has ball hit platform
+        if (
+            platform_pos[0] <= ball_pos[0] <= platform_pos[0] + PLATFORM_WIDTH
+            and platform_pos[1] <= ball_pos[1] <= platform_pos[1] + PLATFORM_HEIGHT
+        ):
+            ball_speed[1] = -ball_speed[1]
+            score += 1
+        #levels
+        if score >= current_level * 10:
+            current_level += 1
+            ball_pos = [WIDTH // 2, HEIGHT // 2]
+            ball_speed = [random.uniform(2, 4), random.uniform(2, 4)]
+            platform_color = change_platform_color()
+
+        #is ball off screen?
+        if ball_pos[1] >= HEIGHT:
+            #decrease lives
+            lives -= 1
+            if lives == 0:
+                game_over()
+                start_screen()
+                score = 0 
+                lives = 3
+                current_level = 1
+            else:
+                #reset ball pos
+                ball_pos = [WIDTH //2 , HEIGHT // 2]
+                #random ball speed
+                ball_speed = [random.uniform(2, 4), random.uniform(2, 4)]
+        #clear screen
+        screen.fill(BLACK)
+
+        #draw the ball
+        pygame.draw.circle(screen, WHITE, (int(ball_pos[0]), int(ball_pos[1])), BALL_RADIUS)
+        #draw the platform
+        pygame.draw.rect(screen,platform_color, (int(platform_pos[0]), int(platform_pos[1]), PLATFORM_WIDTH, PLATFORM_HEIGHT))
+
+        #display the information
+        info_line_y = 10 # vertical pos
+        info_spacing = 75 
+
+        #draw scores in an orange rectangle, top left
+        score_text = font.render(f"score: {score}", True, WHITE)
+        score_rect = score_text.get_rect(topleft = (10, info_line_y))
+        pygame.draw.rect(screen, ORANGE, score_rect.inflate(10, 5))
+        screen.blit(score_text, score_rect)
+
+        #light blue level indicator
+        level_text = font.render(f"Level : {current_level}", True, WHITE)
+        level_rect = level_text.get_rect(topleft = (score_rect.topright[0] + info_spacing, info_line_y))
+        pygame.draw.rect(screen, LIGHT_BLUE, level_rect.inflate(10, 5))
+        screen.blit(level_text, level_rect)
+
+        #draw lives left in red rect
+        lives_text = font.render(f"Lives: {lives}", True, WHITE)
+        lives_rect = lives_text.get_rect(topleft = (level_rect.topright[0] + info_spacing, info_line_y))
+        pygame.draw.rect(screen, RED, lives_rect.inflate(10,5))
+        screen.blit(lives_text, lives_rect)
+
+        pygame.display.flip()
+
+        #fps
+        clock.tick(FPS)
+
+    pygame.quit()
